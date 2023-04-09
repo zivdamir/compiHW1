a51a205fe035d0112aea040a6f111ed8e18bf915
levi776 <levidr776@gmail.com>
2023-04-09 13:10:10 +0300

    hello my frind x#

--- hw1.cpp
+++ hw1.cpp
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include "tokens.hpp"
 extern int yylex();
+#define ARR_SIZE 34
+char* token_array[ARR_SIZE] = {
+    "AUTO",
+    "VOID",
+    "INT",
+    "BYTE",
+    "B",
+    "BOOL",
+    "AND",
+    "OR",
+    "NOT",
+    "TRUE",
+    "FALSE",
+    "RETURN",
+    "IF",
+    "ELSE",
+    "WHILE",
+    "BREAK",
+    "CONTINUE",
+    "SC",
+    "COMMA",
+    "LPAREN",
+    "RPAREN",
+    "LBRACE",
+    "RBRACE",
+    "ASSIGN",
+    "RELOP",
+    "BINOP",
+    "COMMENT",
+	"ID",
+    "NUM",
+    "STRING",
+    "ERROR",
+    "UNCLOSED_STRING",
+    "ERROR_HEX",
+    "ERROR_ESCAPE_SEQUENCE",
+	  
+  };
+void stringHandler(int token)
+{
 
+
+}
 void showToken(const char * name)
 {
         printf("Lex found a %s, the lexeme is %s and its length is %d\n", name, yytext, yyleng);
--- tokens.hpp
+++ tokens.hpp
@@ -1,14 +1,48 @@
 #ifndef TOKENS_HPP_
 #define TOKENS_HPP_
+#define MAX_STR_SIZE 1024
+
   enum tokentype
   {
-    NUM = 1,
-    WORD = 2,
-    EMAIL = 3,
-    VOID = 4,
+    AUTO = 0,
+    VOID = 1,
+    INT = 2,
+    BYTE = 3,
+    B = 4,
+    BOOL = 5,
+    AND = 6,
+    OR = 7,
+    NOT = 8,
+    TRUE = 9,
+    FALSE = 10,
+    RETURN = 11,
+    IF = 12,
+    ELSE = 13,
+    WHILE = 14,
+    BREAK = 15,
+    CONTINUE = 16, 
+    SC = 17,
+    COMMA = 18,
+    LPAREN = 19,
+    RPAREN = 20,
+    LBRACE = 21,
+    RBRACE = 22,
+    ASSIGN = 23,
+    RELOP = 24,
+    BINOP = 25,
+    COMMENT = 26,
+    ID = 27,
+    NUM = 28,
+    STRING = 29,
+    ERROR=30,
+    UNCLOSED_STRING=31,
+    ERROR_HEX=32,
+    ERROR_ESCAPE_SEQUENCE=33,
+	  
   };
   extern int yylineno;
   extern char* yytext;
   extern int yyleng;
   extern int yylex();
+
 #endif /* TOKENS_HPP_ */
